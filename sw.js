05c01a4751aa8102196383e6c724204925558718
room1985 <127143263+room1985@users.noreply.github.com>
2026-03-17 23:31:20 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* NeoCast Service Worker */
-const CACHE = 'neocast-v110';
+const CACHE = 'neocast-v111';
 const SHELL = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
